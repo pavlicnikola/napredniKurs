eb80017683819481318e3c84b960247085ef65e6
pavlicnikola <nikolapavlic2002@gmail.com>
2022-04-10 18:33:57 +0200

    dan8

--- dan8/DOM.js
+++ dan8/DOM.js
@@ -1,6 +1,26 @@
 // Document oobject model
 
+const body = document.body
 const p = document.getElementById('paragraf')
 const div = document.getElementsByClassName('mojDiv')
+const pSvi = document.getElementsByTagName('div')
+            // ili
+const mojH1 = document.querySelector('h1')
+// const mojDiv = document.querySelectorAll('h1')
 
-console.log(div);
+mojH1.id = 'noviID'
+// div.className
+
+// mojH1.textContent = 'Novi naslov'
+// mojH1.innerText = '<i>Novi naslov</i>'
+mojH1.innerHTML = '<i>Novi naslov</i>'
+
+mojH1.style.backgroundColor = '#000'
+mojH1.style.color = '#fff'
+
+const mojP = document.createElement('p')
+mojP.innerText = 'lorem ipsum dolor sit'
+body.appendChild(mojP)
+// body.append(mojP, element2, element3 ...)
+
+console.log(mojH1)
