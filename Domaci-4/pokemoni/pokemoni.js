780d4b30dbe99095b18bacff6df7270842f1eb62
pavlicnikola <nikolapavlic2002@gmail.com>
2022-04-24 23:50:06 +0200

    update

---
+++ Domaci-4/pokemoni/pokemoni.js
@@ -0,0 +1,44 @@
+
+let Pikachu = {
+    ime: 'Pikachu',
+    vrsta: 'elektricni, mis',
+    sposobnosti: 'staticki elektricitet',
+    karakteristike: {
+        napad: 55,
+        odbrana: 40,
+        brzina: 90,
+        }
+    }
+
+let Blastoise = {
+    ime: 'Blastoise',
+    vrsta: 'vodeni, oklopni',
+    sposobnosti: 'vodeni top',
+    karakteristike: {
+        napad: 83,
+        odbrana: 100,
+        brzina: 78,
+        }
+    }
+
+let Totodile = {
+    ime: 'Totodile',
+    vrsta: 'vodeni, celjusni',
+    sposobnosti: '',
+    karakteristike: {
+        napad: 65,
+        odbrana: 64,
+        brzina: 43,
+        }
+    }
+
+let Charizard = {
+    ime: 'Charizard',
+    vrsta: 'vatreni, leteci',
+    sposobnosti: 'plamen',
+    karakteristike: {
+        napad: 84,
+        odbrana: 78,
+        brzina: 100,
+        }
+    }
