78abdad6e5fd75cb6f9f511265611bad73de4dd3
pavlicnikola <nikolapavlic2002@gmail.com>
2022-04-25 18:21:25 +0200

    update

--- Domaci-4/zadatak/domaci.js
+++ Domaci-4/zadatak/domaci.js
@@ -1,14 +1,29 @@
-// random generisanje brojeva u nizu od 1 do 20, a da se ni jedan ne ponovi
+// random generisanje brojeva u nizu od 1 do 20, a da se ni jedan ne ponovi :
+
+// let niz = []
+
+// for (let i = 0; i < 20; i++) {
+//     let generator = Math.floor(Math.random() * 20) + 1
+
+//     while(niz.includes(generator)) {
+//         generator = Math.floor(Math.random() * 20) + 1
+//     }
+
+//     niz.push(generator)
+// }
+//         console.log(niz);
+
+///////////////////////////////////////////////////////////////////////
+
 let niz = []
+let maxNumber = 20
+let randomNumber
 
-for (let i = 0; i < 20; i++) {
-    let generator = Math.floor(Math.random() * 20) + 1
+while(niz.length <= 19) {
+    randomNumber = Math.ceil(Math.random() * maxNumber)
 
-    while(niz.includes(generator)) {
-        generator = Math.floor(Math.random() * 20) + 1
+    if(niz.indexOf(randomNumber) == -1) {
+        niz.push(randomNumber)
     }
-
-    niz.push(generator)
 }
-
-console.log(niz);
+        console.log(niz);
