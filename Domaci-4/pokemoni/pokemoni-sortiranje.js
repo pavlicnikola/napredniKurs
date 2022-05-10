c910f1cbc492e7f2af9fffc55e807d08747594b9
pavlicnikola <nikolapavlic2002@gmail.com>
2022-05-10 18:21:00 +0200

    update

--- Domaci-4/pokemoni/pokemoni-sortiranje.js
+++ Domaci-4/pokemoni/pokemoni-sortiranje.js
@@ -1,48 +1,48 @@
-let Pikachu = {
-    ime: 'Pikachu',
-    vrsta: 'elektricni, mis',
-    sposobnosti: 'staticki elektricitet',
-    karakteristike: {
-        napad: 55,
-        odbrana: 40,
-        brzina: 90,
+let pokemoni = [ 
+    {
+        ime: 'Pikachu',
+        vrsta: 'elektricni, mis',
+        sposobnosti: 'staticki elektricitet',
+        karakteristike: {
+            napad: 55,
+            odbrana: 40,
+            brzina: 90,
         }
-    }
+    },
 
-let Blastoise = {
-    ime: 'Blastoise',
-    vrsta: 'vodeni, oklopni',
-    sposobnosti: 'vodeni top',
-    karakteristike: {
-        napad: 83,
-        odbrana: 100,
-        brzina: 78,
+    {
+        ime: 'Blastoise',
+        vrsta: 'vodeni, oklopni',
+        sposobnosti: 'vodeni top',
+        karakteristike: {
+            napad: 83,
+            odbrana: 100,
+            brzina: 78,
+            }
+    }, 
+
+    {
+        ime: 'Totodile',
+        vrsta: 'vodeni, celjusni',
+        sposobnosti: 'vodeni mlaz, ugriz',
+        karakteristike: {
+            napad: 65,
+            odbrana: 64,
+            brzina: 43,
         }
-    }
+    },
 
-let Totodile = {
-    ime: 'Totodile',
-    vrsta: 'vodeni, celjusni',
-    sposobnosti: 'vodeni mlaz, ugriz',
-    karakteristike: {
-        napad: 65,
-        odbrana: 64,
-        brzina: 43,
-        }
+    {
+        ime: 'Charizard',
+        vrsta: 'vatreni, leteci',
+        sposobnosti: 'plamen',
+        karakteristike: {
+            napad: 84,
+            odbrana: 78,
+            brzina: 100,
+            }
     }
-
-let Charizard = {
-    ime: 'Charizard',
-    vrsta: 'vatreni, leteci',
-    sposobnosti: 'plamen',
-    karakteristike: {
-        napad: 84,
-        odbrana: 78,
-        brzina: 100,
-        }
-    }
-
-let pokemoni = [ Pikachu, Blastoise, Totodile, Charizard ]
+]
 
 const sposobnosti = (niz) => {
     let nizSposobnosti = []
@@ -69,3 +69,5 @@ const najjaci = (niz) => {
 
     return niz.pop()
 }
+
+    console.log(najjaci(pokemoni));
