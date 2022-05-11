b4063b82cdbabbaf51677eb20c73088c2a125c4c
pavlicnikola <nikolapavlic2002@gmail.com>
2022-05-11 17:18:37 +0200

    update

--- dan13/proveraUsername.js
+++ dan13/proveraUsername.js
@@ -1,7 +1,12 @@
 const inputUsername = document.querySelector('#username')
-const button = document.querySelector('#dugme')
+// const button = document.querySelector('#dugme')
 
 let korisnici = [
+    {
+        username:'admin',
+        password:'admin'
+    },
+
     {
         username:'Nikola10',
         password:'Nikola11'
@@ -13,32 +18,41 @@ let korisnici = [
     },
 
     {
-        username:'Djole',
-        password:'Djole_05'
-    },
-
-    {
         username: 'ucenik',
         password: 'ucenik9'
     }
 ]
 
-inputUsername.addEventListener('keypress', (event) => {
-    if(event.key === 'Enter') {
-        let postoji = false
+inputUsername.addEventListener('focusout', (event) => {
+    let postoji = false
     
-        korisnici.forEach( korisnik => {
-            if(inputUsername.value.trim() === korisnik.username) {
-                postoji = true
-            }
-        })
-    
-        if(postoji) {
-            inputUsername.style.borderColor = '#f00'
-        } else {
-            inputUsername.style.borderColor = '#0f0'
-        }
-    } else {
-        console.log(event.key);
-    }
+    korisnici.forEach(korisnik => {
+        if(inputUsername.value.trim() === korisnik.username)
+            postoji = true
+    })
+
+    if(postoji)
+        inputUsername.style.borderColor = '#f00'
+    else
+        inputUsername.style.borderColor = '#0f0'
 })
+
+// inputUsername.addEventListener('keypress', (event) => {
+//     if(event.key === 'Enter') {
+//         let postoji = false
+    
+//         korisnici.forEach( korisnik => {
+//             if(inputUsername.value.trim() === korisnik.username) {
+//                 postoji = true
+//             }
+//         })
+    
+//         if(postoji) {
+//             inputUsername.style.borderColor = '#f00'
+//         } else {
+//             inputUsername.style.borderColor = '#0f0'
+//         }
+//     } else {
+//         console.log(event.key);
+//     }
+// })
