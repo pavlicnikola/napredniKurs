5e7182167112672ccc4faa110a68b7b8eccebad2
Andrea <andrealjubicic283@gmail.com>
2022-05-15 17:51:08 +0200

    dan14/15

---
+++ dan14/15/console.js
@@ -0,0 +1,25 @@
+console.log(
+    '%c Danas je lep dan. %cAli mi ipak ucimo programiranje!',
+    'color: green; background-color: yellow;',
+    'color: red;'
+)
+
+const saberi = () => {
+    console.log('pozdrav iz console.js');
+}
+
+const oduzmi = () => {
+    
+}
+
+const podeli = () => {
+    
+}
+
+const matematika = {
+    saberi,
+    oduzmi,
+    podeli
+}
+        // matematika.saberi()
+export default matematika
