d65bdf0023d28314ba203099225846ce4ad23132
Andrea <andrealjubicic283@gmail.com>
2022-05-15 19:02:52 +0200

    dan16

---
+++ dan16/cookies.js
@@ -0,0 +1,2 @@
+document.cookie = 'name = John; expires = ' + new Date (2022, 6, 15).toUTCString() 
+document.cookie = 'lastName = Smith; expires = ' + new Date (2022, 6, 15).toUTCString()
---
+++ dan16/spreadOperator.js
@@ -0,0 +1,17 @@
+let objekat = {
+    ime: '',
+    prezime:'',
+    godine:''
+}
+
+let objekat2 = {
+    ...objekat,
+    struka:'',
+    statusZaposlenja:'',
+    plata:''
+}
+
+let niz = [1, 2, 3, 4, 5]
+let niz2 = [...niz1, 6, 7, 8]
+
+// lakse je ovako nego da prepisujemo sve prosle vrednosti
