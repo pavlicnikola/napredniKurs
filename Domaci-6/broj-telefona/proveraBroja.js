320c79bd4647922a9aa5d09811a0ef90910b0ce7
pavlicnikola <nikolapavlic2002@gmail.com>
2022-05-24 16:46:56 +0200

    update

--- Domaci-6/broj-telefona/proveraBroja.js
+++ Domaci-6/broj-telefona/proveraBroja.js
@@ -1,1 +1,16 @@
-const inputNumber = document.querySelector('#telnum')
+const inpTelNum = document.querySelector('#telnum')
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    if(inpTelNum.value.trim().length >= 9 && inpTelNum.value.trim().length <= 10 && isNaN(inpTelNum.value.trim()) != true) {
+        alert('pravilno ste uneli telefonski broj')
+    } else {
+        alert('nepravilno unet telefonski broj')
+    }
+
+    setTimeout(() => {
+        inpTelNum.value = ''
+    }, 2000)
+})
