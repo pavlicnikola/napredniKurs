5e78b099e726891c2dcf9426385f03263ca9676d
pavlicnikola <nikolapavlic2002@gmail.com>
2022-05-27 17:40:25 +0200

    dijamant zadatak

--- dan2/dijamant.js
+++ dan2/dijamant.js
@@ -11,3 +11,58 @@
 //     *
 
 let n = 5
+let str = ''
+
+for(let i = 0; i < n - 1; i++) {
+    str = ''
+    for(let j = 1; j <= n; j++) {
+        if(j < n - i) {
+            str += ' '
+        } else if (j == n) {
+            str += '*'
+        } else {
+            str += '**'
+        }
+    }
+        console.log(str);
+}
+
+for (let i = 1; i <= 1; i++) {
+    str = ''
+    if(n % 2 != 0) {
+        for(let j = 1; j <= n; j++) {
+            if(j == Math.ceil(n/2)) {
+                str += 'N'
+            } else if(j == Math.ceil(n/2 - 1)) {
+                str += '* '
+            } else if(j == Math.ceil(n/2 + 1)) {
+                str += ' *'
+            } else {
+                str += '**'
+            }
+        }
+    } else {
+        for(let j = 1; j <= n - 1; j++) {
+            if(j == n/2) {
+                str += ' N '
+            } else {
+                str += '**'
+            }
+        }
+    }
+        console.log(str);
+}
+
+for(let i = n - 2; i >= 0; i--) {
+    str = ''
+    for(let j = 1; j <= n; j++) {
+        if(j < n - i) {
+            str += ' '
+        } else if (j == n) {
+            str += '*'
+        } else {
+            str += '**'
+        }
+    }
+        console.log(str);
+}
